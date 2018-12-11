ab741f764447e54c0fa75264cea9083ae21e52f9
Konstantin <soonts@live.com>
2018-12-11 09:14:45 +0100

    Incomplete

--- avs_dx/DxVisuals/Hlsl/Shader.hpp
+++ avs_dx/DxVisuals/Hlsl/Shader.hpp
@@ -28,11 +28,14 @@ public:
 
 	HRESULT compile( const TSourceData& src )
 	{
+		// Drop the old shader
 		result = nullptr;
 
+		// Generate preprocessor macro values
 		Hlsl::Defines def;
 		CHECK( defines( src, def ) );
 
+		// Compile HLSL into DXBC
 		std::vector<uint8_t> dxbc;
 		CStringA errors;
 		const HRESULT hr = Hlsl::compile( stage, codeTemplate, dxbc, errors, &def );
@@ -42,14 +45,21 @@ public:
 			logError( "Error compiling %s shader %s: %s", shaderName<stage>(), name, errors.operator const char*( ) );
 			return hr;
 		}
+		if( errors.GetLength() > 0 )
+		{
+			errors.Trim();
+			logWarning( "Compilation warnings for %s shader %s: %s", shaderName<stage>(), name, errors.operator const char*( ) );
+		}
 
+		// Upload DXBC to GPU
 		CHECK( createShader( dxbc, result ) );
 
+		// Save the new state
 		m_source = src;
 		return S_OK;
 	}
 
-	inline operator IShader<stage>* ( ) const
+	operator IShader<stage>* ( ) const
 	{
 		return result;
 	}
@@ -57,7 +67,7 @@ public:
 private:
 	const char* const name;
 	const pfnMakeDefines defines;
-	const CStringA codeTemplate;
+	const CStringA& codeTemplate;
 	TSourceData m_source;
 	ShaderPtr<stage> result;
 };

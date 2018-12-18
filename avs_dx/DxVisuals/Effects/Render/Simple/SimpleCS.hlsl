08b2b88ebc43744fae3c926964db36fce536c523
Konstantin <soonts@live.com>
2018-12-18 01:00:35 +0100

    Minor

--- avs_dx/DxVisuals/Effects/Render/Simple/SimpleCS.hlsl
+++ avs_dx/DxVisuals/Effects/Render/Simple/SimpleCS.hlsl
@@ -14,16 +14,15 @@ void main( in uint3 threadId : SV_DispatchThreadID )
     const uint id = threadId.x;
     if( id >= pointsCount )
         return;
-    const float f = (float)id;
-    const float countInv = ( 1.0 / pointsCount );
-    const float2 samplePos = float2( ( f + 0.5f ) * countInv, sampleV );
+    const float countInv = 1.0f / ( pointsCount - 1 );
+    const float f = countInv * ( float )id;
     const bool isWaveform = ( sampleV > 0.5 );
-    float val = sampleVisData( samplePos, isWaveform );
+    float val = sampleVisData( float2( f, sampleV ), isWaveform );
     if( isWaveform )
         val = ( val + 1 ) * 0.5;
 
     float2 pos;
-    pos.x = lerp( -1, 1, f * countInv );
+    pos.x = lerp( -1, 1, f );
     pos.y = lerp( y1y2.x, y1y2.y, val );
     dots[ id ] = pos;
 }

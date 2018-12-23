decceeddb007a86e7cb57bfae5da40775149e77b
Konstantin <soonts@live.com>
2018-12-23 02:07:54 +0100

    Blend shader bugfix

--- avs_dx/DxVisuals/Effects/BlendPS.hlsl
+++ avs_dx/DxVisuals/Effects/BlendPS.hlsl
@@ -12,11 +12,11 @@ Texture2D<float4> texDest : register(BIND_DEST);
 
 float4 main( float4 screenSpace : SV_Position ) : SV_Target
 {
-    const int3 pos = int3( int2( screenSpace.xy ), 0 );
+    const int3 pos = int3( int2( screenSpace.xy ), 0 ); // The last component specifies the mipmap level.
     const float4 s = texSource.Load( pos );
-    const float4 d = texSource.Load( pos );
+    const float4 d = texDest.Load( pos );
 
-	// `blend` will become macro in runtime, i.e. these branches are free, and hopefully, HLSL compiler will even optimize away unused texture loads.
+	// `blend` will become macro in runtime, i.e. these branches are free. And hopefully, HLSL compiler will even optimize away unused texture loads for cases 0 and 1.
     switch( blend )
     {
         case 0: // Ignore
@@ -37,7 +37,7 @@ float4 main( float4 screenSpace : SV_Position ) : SV_Target
             return ( 0 == ( (uint) pos.y & 1 ) ) ? s : d;
         case 8: // Every other pixel
             {
-                const uint2 u = (uint2) pos;
+                const uint2 u = (uint2) pos.xy;
                 return ( ( u.x ^ u.y ) & 1 ) ? s : d;
             }
         case 9: // XOR
@@ -45,7 +45,7 @@ float4 main( float4 screenSpace : SV_Position ) : SV_Target
                 const uint3 su = (uint3) ( s.xyz * 255.0 );
                 const uint3 du = (uint3) ( d.xyz * 255.0 );
                 const uint3 x = su ^ du;
-                return float4( (float3) x * ( 1.0 / 255.0 ), 1 );
+                return float4( ( (float3) x ) * ( 1.0 / 255.0 ), 1 );
             }
         case 10: // Adjustable
             return s * blendVal + d * ( 1.0 - blendVal );

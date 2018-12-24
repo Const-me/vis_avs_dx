7b0254584dd2c686d34f3f18c13fa747549b69fc
Konstantin <soonts@live.com>
2018-12-24 15:39:06 +0100

    Optimization, linker settings

--- avs/vis_avs/r_defs.h
+++ avs/vis_avs/r_defs.h
@@ -38,7 +38,8 @@ class C_RBASE {
 public:
 	C_RBASE() { }
 	virtual ~C_RBASE() { };
-	virtual int render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) = 0; // returns 1 if fbout has dest
+	// Was abstract virtual
+	int render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { return 0; }; // returns 1 if fbout has dest
 	virtual HWND conf( HINSTANCE hInstance, HWND hwndParent ) { return 0; };
 	virtual char *get_desc() = 0;
 	virtual void load_config( unsigned char *data, int len ) { }
@@ -57,14 +58,14 @@ public:
 
 	int getRenderVer2() { return 2; }
 
-
-	virtual int smp_getflags() { return 0; } // return 1 to enable smp support
+	// Were virtuals
+	int smp_getflags() { return 0; } // return 1 to enable smp support
 
 	// returns # of threads you desire, <= max_threads, or 0 to not do anything
 	// default should return max_threads if you are flexible
-	virtual int smp_begin( int max_threads, char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { return 0; }
-	virtual void smp_render( int this_thread, int max_threads, char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { };
-	virtual int smp_finish( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { return 0; }; // return value is that of render() for fbstuff etc
+	int smp_begin( int max_threads, char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { return 0; }
+	void smp_render( int this_thread, int max_threads, char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { };
+	int smp_finish( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h ) { return 0; }; // return value is that of render() for fbstuff etc
 
 };
 

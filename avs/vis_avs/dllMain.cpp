13e94626a419633aecf52fa4533d677263935825
Konstantin <soonts@live.com>
2018-12-25 19:27:42 +0100

    Fixed DllMain issue

---
+++ avs/vis_avs/dllMain.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+BOOL __stdcall interopLibDllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved );
+
+BOOL __stdcall DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
+{
+	return interopLibDllMain( hModule, ul_reason_for_call, lpReserved );
+}
--- avs_dx/InteropLib/module.cpp
+++ avs_dx/InteropLib/module.cpp
@@ -4,11 +4,10 @@ class CModule : public CAtlDllModuleT<CModule> { };
 
 CModule _AtlModule;
 
-BOOL __stdcall DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
+// Apparently, VC++ linker only uses DllMain from the main DLL project, ignoring what's in the static libraries.
+// That's why calling this explicitly, from vis_avs\dllMain.cpp
+BOOL __stdcall interopLibDllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
 {
-	// This function is not linked and not called. Probably good idea to fix, maybe with /ENTRY linker switch:
-	// https://docs.microsoft.com/en-us/cpp/build/reference/entry-entry-point-symbol?view=vs-2017
-
 	if( DLL_PROCESS_ATTACH == ul_reason_for_call )
 		DisableThreadLibraryCalls( hModule );
 	return _AtlModule.DllMain( ul_reason_for_call, lpReserved );

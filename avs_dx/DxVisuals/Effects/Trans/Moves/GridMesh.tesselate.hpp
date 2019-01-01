834e3c87f3590a77583213d08117a6d3b7c79cf6
Konstantin <soonts@live.com>
2019-01-01 02:04:25 +0100

    Minor refactor (moved a class)

--- avs_dx/DxVisuals/Effects/Trans/Moves/GridMesh.tesselate.hpp
+++ avs_dx/DxVisuals/Effects/Trans/Moves/GridMesh.tesselate.hpp
@@ -1,49 +1,9 @@
 #pragma once
 #include <Utils/meshoptimizer/meshoptimizer.h>
+#include <Utils/DynamicArray.hpp>
 
 namespace
 {
-	// Very similar to std::array but can contain less elements.
-	template<class T, uint8_t size>
-	class DynamicArray
-	{
-		std::array<T, size> m_data;
-		uint8_t m_count = 0;
-
-	public:
-		DynamicArray() = default;
-		DynamicArray( const DynamicArray &that ) = default;
-		DynamicArray( T t )
-		{
-			m_count = 1;
-			m_data[ 0 ] = t;
-		}
-
-		void add( T v )
-		{
-			assert( m_count < size );
-			m_data[ m_count ] = v;
-			m_count++;
-		}
-		bool addIfNotThere( T newVal )
-		{
-			for( T v : *this )
-				if( v == newVal )
-					return false;
-			add( newVal );
-			return true;
-		}
-
-		decltype( auto ) begin() const
-		{
-			return m_data.begin();
-		};
-		decltype( auto ) end() const
-		{
-			return m_data.begin() + m_count;
-		};
-	};
-
 	// Some parts of the algorithm need to place data in a hash map with triangle edge being the key. This function makes such keys by sorting indices of the two vertices.
 	inline uint32_t edgeId( uint16_t v1, uint16_t v2 )
 	{
---
+++ avs_dx/DxVisuals/Utils/DynamicArray.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+// Very similar to std::array but can contain less elements.
+template<class T, uint8_t capacity>
+class DynamicArray
+{
+	std::array<T, capacity> m_data;
+	uint8_t m_count = 0;
+
+public:
+	DynamicArray() = default;
+	DynamicArray( const DynamicArray &that ) = default;
+	DynamicArray( T t )
+	{
+		m_count = 1;
+		m_data[ 0 ] = t;
+	}
+
+	void add( T v )
+	{
+		assert( m_count < capacity );
+		m_data[ m_count ] = v;
+		m_count++;
+	}
+
+	bool addIfNotThere( T newVal )
+	{
+		for( T v : *this )
+			if( v == newVal )
+				return false;
+		add( newVal );
+		return true;
+	}
+
+	decltype( auto ) begin() const
+	{
+		return m_data.begin();
+	};
+
+	decltype( auto ) end() const
+	{
+		return m_data.begin() + m_count;
+	};
+
+	uint8_t size() const
+	{
+		return m_count;
+	}
+};

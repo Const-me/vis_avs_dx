d141cc19b25044beaa4cd51ac9fcb03b52848242
Konstantin <soonts@live.com>
2019-01-05 03:42:38 +0100

    More intensive frame fade when drawing the list over and over

--- avs_dx/DxVisualsShaders/FadeFramePS.hlsl
+++ avs_dx/DxVisualsShaders/FadeFramePS.hlsl
@@ -4,7 +4,7 @@ float4 main( float4 screenSpace : SV_Position ) : SV_Target
 {
     const int3 readPos = int3( int2( screenSpace.xy ), 0 );
     float4 c = texSource.Load( readPos );
-    const float fadeFactor = 1.0 / 255;
-    c = c * ( 1.0 + fadeFactor ) - fadeFactor;
+    const float fadeFactor = 1.0 / 255.0;
+    c -= fadeFactor;
     return saturate( c );
 }

cb97dc2be11397c2c46dd009c7bf29b7694341ea
Konstantin <soonts@live.com>
2019-01-06 03:26:57 +0100

    Dot fountain cleanup

--- avs/vis_avs/r_dotfnt.cpp
+++ avs/vis_avs/r_dotfnt.cpp
@@ -32,30 +32,16 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "resource.h"
 #include "r_defs.h"
 
-
-#ifndef LASER 
-
 #define C_THISCLASS C_DotFountainClass
 #define MOD_NAME "Render / Dot Fountain"
 
-#define NUM_ROT_DIV 30
-#define NUM_ROT_HEIGHT 256
-typedef struct {
-	float r, dr;
-	float h, dh;
-	float ax, ay;
-	int c;
-} FountainPoint;
-
-class C_THISCLASS : public C_RBASE {
+class C_THISCLASS : public C_RBASE
+{
 protected:
 	float r;
-	FountainPoint points[ NUM_ROT_HEIGHT ][ NUM_ROT_DIV ];
-	int color_tab[ 64 ];
 public:
 	C_THISCLASS();
 	virtual ~C_THISCLASS();
-	virtual int render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int, int );
 	virtual char *get_desc() { return MOD_NAME; }
 	virtual HWND conf( HINSTANCE hInstance, HWND hwndParent );
 	virtual void load_config( unsigned char *data, int len );
@@ -63,10 +49,7 @@ public:
 
 	int rotvel, angle;
 	int colors[ 5 ];
-
-	void initcolortab();
 };
-
 
 #define PUT_INT(y) data[pos]=(y)&255; data[pos+1]=(y>>8)&255; data[pos+2]=(y>>16)&255; data[pos+3]=(y>>24)&255
 #define GET_INT() (data[pos]|(data[pos+1]<<8)|(data[pos+2]<<16)|(data[pos+3]<<24))
@@ -82,12 +65,9 @@ void C_THISCLASS::load_config( unsigned char *data, int len )
 	}
 	if( len - pos >= 4 ) { angle = GET_INT(); pos += 4; }
 	if( len - pos >= 4 ) { int rr = GET_INT(); pos += 4; r = rr / 32.0f; }
-
-
-	initcolortab();
 }
 
-int  C_THISCLASS::save_config( unsigned char *data )
+int C_THISCLASS::save_config( unsigned char *data )
 {
 	int rr;
 	int pos = 0;
@@ -103,26 +83,6 @@ int  C_THISCLASS::save_config( unsigned char *data )
 	return pos;
 }
 
-
-void C_THISCLASS::initcolortab()
-{
-	int x, r, g, b, dr, dg, db, t;
-	for( t = 0; t < 4; t++ )
-	{
-		r = ( colors[ t ] & 255 ) << 16;
-		g = ( ( colors[ t ] >> 8 ) & 255 ) << 16;
-		b = ( ( colors[ t ] >> 16 ) & 255 ) << 16;
-		dr = ( ( ( colors[ t + 1 ] & 255 ) - ( colors[ t ] & 255 ) ) << 16 ) / 16;
-		dg = ( ( ( ( colors[ t + 1 ] >> 8 ) & 255 ) - ( ( colors[ t ] >> 8 ) & 255 ) ) << 16 ) / 16;
-		db = ( ( ( ( colors[ t + 1 ] >> 16 ) & 255 ) - ( ( colors[ t ] >> 16 ) & 255 ) ) << 16 ) / 16;
-		for( x = 0; x < 16; x++ )
-		{
-			color_tab[ t * 16 + x ] = ( r >> 16 ) | ( ( g >> 16 ) << 8 ) | ( ( b >> 16 ) << 16 );
-			r += dr; g += dg; b += db;
-		}
-	}
-}
-
 C_THISCLASS::C_THISCLASS()
 {
 	colors[ 0 ] = RGB( 24, 107, 28 ); // reverse BGR :)
@@ -130,9 +90,6 @@ C_THISCLASS::C_THISCLASS()
 	colors[ 2 ] = RGB( 116, 29, 42 );
 	colors[ 3 ] = RGB( 217, 54, 144 );
 	colors[ 4 ] = RGB( 255, 136, 107 );
-	initcolortab();
-
-	memset( points, 0, sizeof( points ) );
 
 	angle = -20;
 	rotvel = 16;
@@ -145,113 +102,11 @@ C_THISCLASS::~C_THISCLASS()
 }
 
 
-int C_THISCLASS::render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int width, int height )
-{
-	if( isBeat & 0x80000000 ) return 0;
-	int fo, p;
-	float matrix[ 16 ], matrix2[ 16 ];
-	matrixRotate( matrix, 2, r );
-	matrixRotate( matrix2, 1, (float)angle );
-	matrixMultiply( matrix, matrix2 );
-	matrixTranslate( matrix2, 0.0f, -20.0f, 400.0f );
-	matrixMultiply( matrix, matrix2 );
-
-
-	FountainPoint pb[ NUM_ROT_DIV ];
-	FountainPoint *in, *out;
-	memcpy( pb, &points[ 0 ], sizeof( pb ) );
-	fo = NUM_ROT_HEIGHT - 2;
-	do      // transform points and remove old ones
-	{
-		float booga = 1.3f / ( fo + 100 );
-		in = &points[ fo ][ 0 ];
-		out = &points[ fo + 1 ][ 0 ];
-		for( p = 0; p < NUM_ROT_DIV; p++ )
-		{
-			*out = *in;
-			out->r += out->dr;
-			out->dh += 0.05f;
-			out->dr += booga;
-			out->h += out->dh;
-			out++;
-			in++;
-		}
-	} while( fo-- );
-
-	out = &points[ 0 ][ 0 ];
-	in = pb;
-	{ // create new points
-		float a;
-		unsigned char *sd = (unsigned char *)visdata[ 1 ][ 0 ];
-		for( p = 0; p < NUM_ROT_DIV; p++ )
-		{
-			int t;
-			if( p >= NUM_ROT_DIV ) t = ( sd[ 576 ] ^ 128 );
-			else t = ( *sd ^ 128 );
-			sd++;
-			t *= 5;
-			t /= 4;
-			t -= 64;
-			if( isBeat ) t += 128;
-			if( t > 255 ) t = 255;
-			//      t+=sd[576]^128;
-			  //    t/=2;
-			out->r = 1.0f;
-			float dr = t / 200.0f;
-			if( dr < 0 ) dr = -dr;
-			out->h = 250;
-			dr += 1.0;
-			out->dh = -dr * ( 100.0f + ( out->dh - in->dh ) ) / 100.0f * 2.8f;
-			t = t / 4;
-			if( t > 63 ) t = 63;
-			out->c = color_tab[ t ];
-			a = p * 3.14159f * 2.0f / NUM_ROT_DIV;
-			out->ax = (float)sin( a );
-			out->ay = (float)cos( a );
-			out->dr = 0.0;
-			out++;
-			in++;
-		}
-	}
-
-	float adj = width * 440.0f / 640.0f;
-	float adj2 = height * 440.0f / 480.0f;
-	if( adj2 < adj ) adj = adj2;
-	in = &points[ 0 ][ 0 ];
-	for( fo = 0; fo < NUM_ROT_HEIGHT; fo++ )
-	{
-		for( p = 0; p < NUM_ROT_DIV; p++ )
-		{
-			register float x, y, z;
-			matrixApply( matrix, in->ax*in->r, in->h, in->ay*in->r, &x, &y, &z );
-			z = adj / z;
-			if( z > 0.0000001 )
-			{
-				register int ix = (int)( x * z ) + width / 2;
-				register int iy = (int)( y * z ) + height / 2;
-				if( iy >= 0 && iy < height && ix >= 0 && ix < width )
-				{
-					BLEND_LINE( framebuffer + iy * width + ix, in->c );
-				}
-			}
-			in++;
-		}
-	}
-	r += rotvel / 5.0f;
-	if( r >= 360.0f ) r -= 360.0f;
-	if( r < 0.0f ) r += 360.0f;
-	return 0;
-}
-
 C_RBASE *R_DotFountain( char *desc )
 {
 	if( desc ) { strcpy( desc, MOD_NAME ); return NULL; }
 	return ( C_RBASE * ) new C_THISCLASS();
 }
-
-
-
-
 
 static C_THISCLASS *g_this;
 
@@ -269,7 +124,6 @@ static BOOL CALLBACK g_DlgProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM l
 		if( LOWORD( wParam ) >= IDC_C1 && LOWORD( wParam ) <= IDC_C5 ) {
 			GR_SelectColor( hwndDlg, &g_this->colors[ IDC_C5 - LOWORD( wParam ) ] );
 			InvalidateRect( GetDlgItem( hwndDlg, LOWORD( wParam ) ), NULL, FALSE );
-			g_this->initcolortab();
 		}
 		return 0;
 	case WM_DRAWITEM:
@@ -308,14 +162,8 @@ static BOOL CALLBACK g_DlgProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM l
 	return 0;
 }
 
-
 HWND C_THISCLASS::conf( HINSTANCE hInstance, HWND hwndParent )
 {
 	g_this = this;
 	return CreateDialog( hInstance, MAKEINTRESOURCE( IDD_CFG_DOTPLANE ), hwndParent, g_DlgProc );
 }
-
-#else
-C_RBASE *R_DotFountain( char *desc ) { return NULL; }
-
-#endif

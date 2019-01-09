71712b166e95415be3680c19a3010449009571e0
Konstantin <soonts@live.com>
2019-01-09 09:54:02 +0100

    Superscope cleanup

--- avs/vis_avs/r_sscope.cpp
+++ avs/vis_avs/r_sscope.cpp
@@ -48,7 +48,6 @@ protected:
 public:
 	C_THISCLASS();
 	virtual ~C_THISCLASS();
-	int render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h );
 	virtual char *get_desc() { return MOD_NAME; }
 	virtual HWND conf( HINSTANCE hInstance, HWND hwndParent );
 	virtual void load_config( unsigned char *data, int len );
@@ -58,16 +57,6 @@ public:
 	int num_colors;
 	int colors[ 16 ];
 	int mode;
-
-	int color_pos;
-
-	int AVS_EEL_CONTEXTNAME;
-	double *var_b, *var_x, *var_y, *var_i, *var_n, *var_v, *var_w, *var_h, *var_red, *var_green, *var_blue;
-	double *var_skip, *var_linesize, *var_drawmode;
-	int inited;
-	int codehandle[ 4 ];
-	int need_recompile;
-	CRITICAL_SECTION rcs;
 };
 
 #define PUT_INT(y) data[pos]=(y)&255; data[pos+1]=(y>>8)&255; data[pos+2]=(y>>16)&255; data[pos+3]=(y>>24)&255
@@ -106,7 +95,6 @@ void C_THISCLASS::load_config( unsigned char *data, int len )
 	if( num_colors <= 16 ) while( len - pos >= 4 && x < num_colors ) { colors[ x++ ] = GET_INT(); pos += 4; }
 	else num_colors = 0;
 	if( len - pos >= 4 ) { mode = GET_INT(); pos += 4; }
-	need_recompile = 1;
 }
 
 int  C_THISCLASS::save_config( unsigned char *data )
@@ -125,218 +113,35 @@ int  C_THISCLASS::save_config( unsigned char *data )
 	return pos;
 }
 
-
-
 C_THISCLASS::C_THISCLASS()
 {
-	InitializeCriticalSection( &rcs );
-	AVS_EEL_INITINST();
 #ifdef LASER
 	mode = 1;
 #else
 	mode = 0;
 #endif
 
-
-	need_recompile = 1;
 	which_ch = 2;
 	num_colors = 1;
 	memset( colors, 0, sizeof( colors ) );
 	colors[ 0 ] = RGB( 255, 255, 255 );
-	color_pos = 0;
-	memset( codehandle, 0, sizeof( codehandle ) );
 
-#ifdef LASER
-	effect_exp[ 0 ].assign( "d=i+v*0.2; r=t+i*$PI*4; x=cos(r)*d; y=sin(r)*d" );
-	effect_exp[ 1 ].assign( "t=t-0.05" );
-	effect_exp[ 2 ].assign( "" );
-	effect_exp[ 3 ].assign( "n=100" );
-#else
 	effect_exp[ 0 ].assign( "d=i+v*0.2; r=t+i*$PI*4; x=cos(r)*d; y=sin(r)*d" );
 	effect_exp[ 1 ].assign( "t=t-0.05" );
 	effect_exp[ 2 ].assign( "" );
 	effect_exp[ 3 ].assign( "n=800" );
-#endif
-
-	var_n = 0;
 
 	CREATE_DX_EFFECT( effect_exp );
 }
 
 C_THISCLASS::~C_THISCLASS()
-{
-	int x;
-	for( x = 0; x < 4; x++ )
-	{
-		freeCode( codehandle[ x ] );
-		codehandle[ x ] = 0;
-	}
-	AVS_EEL_QUITINST();
-	DeleteCriticalSection( &rcs );
-}
+{ }
 
 static __inline int makeint( double t )
 {
 	if( t <= 0.0 ) return 0;
 	if( t >= 1.0 ) return 255;
 	return (int)( t*255.0 );
-}
-
-int C_THISCLASS::render( char visdata[ 2 ][ 2 ][ 576 ], int isBeat, int *framebuffer, int *fbout, int w, int h )
-{
-	if( need_recompile )
-	{
-		EnterCriticalSection( &rcs );
-
-		if( !var_n || g_reset_vars_on_recompile )
-		{
-			clearVars();
-			var_n = registerVar( "n" );
-			var_b = registerVar( "b" );
-			var_x = registerVar( "x" );
-			var_y = registerVar( "y" );
-			var_i = registerVar( "i" );
-			var_v = registerVar( "v" );
-			var_w = registerVar( "w" );
-			var_h = registerVar( "h" );
-			var_red = registerVar( "red" );
-			var_green = registerVar( "green" );
-			var_blue = registerVar( "blue" );
-			var_linesize = registerVar( "linesize" );
-			var_skip = registerVar( "skip" );
-			var_drawmode = registerVar( "drawmode" );
-			*var_n = 100.0;
-			inited = 0;
-		}
-
-		need_recompile = 0;
-		int x;
-		for( x = 0; x < 4; x++ )
-		{
-			freeCode( codehandle[ x ] );
-			codehandle[ x ] = compileCode( effect_exp[ x ].get() );
-		}
-
-		LeaveCriticalSection( &rcs );
-	}
-	if( isBeat & 0x80000000 ) return 0;
-
-	if( !num_colors ) return 0;
-
-	int x;
-	int current_color;
-	unsigned char *fa_data;
-	char center_channel[ 576 ];
-	int ws = ( which_ch & 4 ) ? 1 : 0;
-	int xorv = ( ws * 128 ) ^ 128;
-
-	if( ( which_ch & 3 ) >= 2 )
-	{
-		for( x = 0; x < 576; x++ ) center_channel[ x ] = visdata[ ws ^ 1 ][ 0 ][ x ] / 2 + visdata[ ws ^ 1 ][ 1 ][ x ] / 2;
-		fa_data = (unsigned char *)center_channel;
-	}
-	else fa_data = (unsigned char *)&visdata[ ws ^ 1 ][ which_ch & 3 ][ 0 ];
-
-	color_pos++;
-	if( color_pos >= num_colors * 64 ) color_pos = 0;
-
-	{
-		int p = color_pos / 64;
-		int r = color_pos & 63;
-		int c1, c2;
-		int r1, r2, r3;
-		c1 = colors[ p ];
-		if( p + 1 < num_colors )
-			c2 = colors[ p + 1 ];
-		else c2 = colors[ 0 ];
-
-		r1 = ( ( ( c1 & 255 )*( 63 - r ) ) + ( ( c2 & 255 )*r ) ) / 64;
-		r2 = ( ( ( ( c1 >> 8 ) & 255 )*( 63 - r ) ) + ( ( ( c2 >> 8 ) & 255 )*r ) ) / 64;
-		r3 = ( ( ( ( c1 >> 16 ) & 255 )*( 63 - r ) ) + ( ( ( c2 >> 16 ) & 255 )*r ) ) / 64;
-
-		current_color = r1 | ( r2 << 8 ) | ( r3 << 16 );
-	}
-
-	*var_h = h;
-	*var_w = w;
-	*var_b = isBeat ? 1.0 : 0.0;
-	*var_blue = ( current_color & 0xff ) / 255.0;
-	*var_green = ( ( current_color >> 8 ) & 0xff ) / 255.0;
-	*var_red = ( ( current_color >> 16 ) & 0xff ) / 255.0;
-	*var_skip = 0.0;
-	*var_linesize = (double)( ( g_line_blend_mode & 0xff0000 ) >> 16 );
-	*var_drawmode = mode ? 1.0 : 0.0;
-	if( codehandle[ 3 ] && !inited ) { executeCode( codehandle[ 3 ], visdata ); inited = 1; }
-	executeCode( codehandle[ 1 ], visdata );
-	if( isBeat ) executeCode( codehandle[ 2 ], visdata );
-	if( codehandle[ 0 ] )
-	{
-		int candraw = 0, lx = 0, ly = 0;
-#ifdef LASER
-		double dlx = 0.0, dly = 0.0;
-#endif
-		int a;
-		int l = (int)*var_n;
-		if( l > 128 * 1024 ) l = 128 * 1024;
-		for( a = 0; a < l; a++ )
-		{
-			int x, y;
-			double r = ( a*576.0 ) / l;
-			double s1 = r - (int)r;
-			double yr = ( fa_data[ (int)r ] ^ xorv )*( 1.0f - s1 ) + ( fa_data[ (int)r + 1 ] ^ xorv )*( s1 );
-			*var_v = yr / 128.0 - 1.0;
-			*var_i = (double)a / (double)( l - 1 );
-			*var_skip = 0.0;
-			executeCode( codehandle[ 0 ], visdata );
-			x = (int)( ( *var_x + 1.0 )*w*0.5 );
-			y = (int)( ( *var_y + 1.0 )*h*0.5 );
-			if( *var_skip < 0.00001 )
-			{
-				int thiscolor = makeint( *var_blue ) | ( makeint( *var_green ) << 8 ) | ( makeint( *var_red ) << 16 );
-				if( *var_drawmode < 0.00001 )
-				{
-					if( y >= 0 && y < h && x >= 0 && x < w )
-					{
-#ifdef LASER
-						laser_drawpoint( (float)*var_x, (float)*var_y, thiscolor );
-#else
-						BLEND_LINE( framebuffer + x + y * w, thiscolor );
-#endif
-					}
-				}
-				else
-				{
-					if( candraw )
-					{
-#ifdef LASER
-						LineType l;
-						l.color = thiscolor;
-						l.mode = 0;
-						l.x1 = (float)*var_x;
-						l.y1 = (float)*var_y;
-						l.x2 = (float)dlx;
-						l.y2 = (float)dly;
-						g_laser_linelist->AddLine( &l );
-#else
-						if( ( thiscolor & 0xffffff ) || ( g_line_blend_mode & 0xff ) != 1 )
-						{
-							line( framebuffer, lx, ly, x, y, w, h, thiscolor, (int)( *var_linesize + 0.5 ) );
-						}
-#endif
-					} // candraw
-				} // line
-			} // skip
-			candraw = 1;
-			lx = x;
-			ly = y;
-#ifdef LASER
-			dlx = *var_x;
-			dly = *var_y;
-#endif
-		}
-	}
-
-	return 0;
 }
 
 C_RBASE *R_SScope( char *desc )
@@ -454,14 +259,11 @@ BOOL CALLBACK C_THISCLASS::g_DlgProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LP
 		{
 			if( ( LOWORD( wParam ) == IDC_EDIT1 || LOWORD( wParam ) == IDC_EDIT2 || LOWORD( wParam ) == IDC_EDIT3 || LOWORD( wParam ) == IDC_EDIT4 ) && HIWORD( wParam ) == EN_CHANGE )
 			{
-				EnterCriticalSection( &g_this->rcs );
+				UPDATE_PARAMS();
 				g_this->effect_exp[ 0 ].get_from_dlgitem( hwndDlg, IDC_EDIT1 );
 				g_this->effect_exp[ 1 ].get_from_dlgitem( hwndDlg, IDC_EDIT2 );
 				g_this->effect_exp[ 2 ].get_from_dlgitem( hwndDlg, IDC_EDIT3 );
 				g_this->effect_exp[ 3 ].get_from_dlgitem( hwndDlg, IDC_EDIT4 );
-				g_this->need_recompile = 1;
-				if( LOWORD( wParam ) == IDC_EDIT4 ) g_this->inited = 0;
-				LeaveCriticalSection( &g_this->rcs );
 #if 0//syntax highlighting
 				if( LOWORD( wParam ) == IDC_EDIT1 )
 					doAVSEvalHighLight( hwndDlg, IDC_EDIT1, g_this->effect_exp[ 0 ].get() );
@@ -577,7 +379,6 @@ BOOL CALLBACK C_THISCLASS::g_DlgProc( HWND hwndDlg, UINT uMsg, WPARAM wParam, LP
 	}
 	return 0;
 }
-
 
 HWND C_THISCLASS::conf( HINSTANCE hInstance, HWND hwndParent )
 {
